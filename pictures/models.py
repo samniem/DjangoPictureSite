d2e69b87d5ddfd9455ee2e68a191c3504053203b
samniem <sami.nieminen.hw@gmail.com>
2019-03-27 22:00:16 +0200

    Minor update.

--- pictures/models.py
+++ pictures/models.py
@@ -5,12 +5,12 @@ from datetime import datetime
 
 # Create your models here.
 class Pictures(models.Model):
-    #name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='pictures/static/pictures/pix', default=str,
     null=bool)
     loaddress = photo.name
-    #upload_time = models.DateTimeField(default=datetime.now, blank=True)
-    #def __str__(self):
-    #    return self.photo
+    upload_time = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return self.photo
     class Meta:
         verbose_name_plural = "Pictures"
